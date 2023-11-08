f4151682f98856555bbcc0c6ca7dc5324c9cf21a
Hugues Collet <hc.pro@outlook.fr>
2023-11-08 13:47:54 +0100

    Debut du responsive

---
+++ app.js
@@ -0,0 +1,24 @@
+const logoMenu = document.querySelector(".logo-menu-img");
+const ul = document.querySelector("ul");
+const li = document.querySelectorAll("li");
+
+logoMenu.addEventListener("click", () => {
+  if (ul.className === "") {
+    ul.className += "responsive";
+    logoMenu.style.width = "30px";
+    logoMenu.style.top = "5px";
+    logoMenu.src = "./ressources/close.png";
+  } else {
+    ul.className = "";
+    logoMenu.style.width = "30px";
+    logoMenu.style.top = "13px";
+    logoMenu.src = "./ressources/menu-icon.png";
+  }
+});
+
+li.forEach((item) => {
+  item.addEventListener("click", () => {
+    ul.className = "";
+    logoMenu.src = "ressources/menu-icon.png";
+  });
+});
